846693c28ffb88ca44308f3652a84641354e9d6c
Vaibhav Sharma <vaibhav@umn.edu>
2016-02-08 23:20:06 -0600

    Adding code to test 3NN identification accuracy when the number of users is changed from 10 to 42

---
+++ inc_users_knn/main.m
@@ -0,0 +1,74 @@
+%clear;
+button_k=8;
+checkbox_k=8;
+radiobutton_k=8;
+togglebutton_k=8;
+switch_k=8;
+edittext_k=8;
+spinner_k=8;
+spinner_button_k=8;
+picker_k=8;
+test_err=[];
+
+NumNeighbors=3;
+test_err=[];
+n_classes=42;
+
+%fs from ens_svm_1c_wt
+fs=[
+'11110000001', %button
+'11110000001', %checkbox
+'11100000001', %radiobutton
+'11100000001', %togglebutton
+'11110100001', %switch
+'10100000001', %edittext
+'10100000001', %spinner
+'11110000001', %spinner-button
+'11110000011', %picker %'11111100001' used in gda_5
+];
+
+fs(9,:)='11111100001';
+
+test_all_err=[];
+
+count=10000;
+for n_classes=10:42
+  test_err=[];
+  for test_set=1:5
+    button_result = knn_all('button','knn',button_k,test_set,fs(1,:),NumNeighbors,n_classes,count);
+    checkbox_result = knn_all('checkbox','knn',checkbox_k,test_set,fs(2,:),NumNeighbors,n_classes,count);
+    radiobutton_result = knn_all('radiobutton','knn',radiobutton_k,test_set,fs(3,:),NumNeighbors,n_classes,count);
+    %togglebutton_result = knn_all('togglebutton','knn',togglebutton_k,test_set,fs(4,:),NumNeighbors,n_classes,count);
+    switch_result = knn_all('switch','knn',switch_k,test_set,fs(5,:),NumNeighbors,n_classes,count);
+    edittext_result = knn_all('edittext','knn',edittext_k,test_set,fs(6,:),NumNeighbors,n_classes,count);
+    spinner_result = knn_all('spinner','knn',spinner_k,test_set,fs(7,:),NumNeighbors,n_classes,count);
+    spinner_button_result = knn_all('spinner-button','knn',spinner_button_k,test_set,fs(8,:),NumNeighbors,n_classes,count);
+    picker_result = knn_all('picker','knn',picker_k,test_set,fs(9,:),NumNeighbors,n_classes,count);
+    
+    final_result = [button_result checkbox_result radiobutton_result ...
+                    switch_result edittext_result ...
+                    spinner_result spinner_button_result picker_result];
+    
+    emp_err = 0;
+    for i=1:size(final_result,1)
+      final_result(i,:)=sort(final_result(i,:));
+      if mode(final_result(i,:)) ~= i
+        emp_err = emp_err + 1;
+      end
+    end
+    test_err=[test_err;emp_err];
+  end
+  test_all_err=[test_all_err test_err];
+
+
+end
+
+test_mean_err=mean(test_all_err);
+
+%logging to log file
+fileID=fopen('inc_inter_knn.log','a');
+fprintf(fileID,'test_mean_err=\n');
+fprintf(fileID,'%d ',test_mean_err);
+fprintf(fileID,'\n');
+fclose(fileID);
+
---
+++ inc_users_knn/multiClass_knn_all.m
@@ -0,0 +1,77 @@
+function topKResult = multiClass_knn_all(train_sizes,train_data, ...
+                                     test_sizes,test_data,K,NumNeighbors),
+  classifier_count=0;
+  training_set_1=[];
+  training_set_2=[];
+  predictions=[];
+  test_size=size(test_data,1);
+  n_classes=size(test_sizes,1);
+  votes=zeros(test_size,(n_classes*(n_classes-1))/2);
+  topKResult = [];
+  
+  if K > n_classes
+    K = n_classes
+  end
+
+  for i=1:n_classes
+    training_set_1=getTrainingSet(train_sizes,train_data,i,4);
+    for j=i+1:n_classes
+      %fprintf('Comparing %d & %d\n',i,j);
+      training_set_2=getTrainingSet(train_sizes,train_data,j,4);
+      classifier_count = classifier_count+1;
+      predictions = predclass_knn_all(training_set_1,training_set_2,test_data,NumNeighbors);
+      for k=1:test_size
+        if predictions(k,1) == 1 
+          votes(k,classifier_count)=i;
+        end
+        if predictions(k,1) == 2
+          votes(k,classifier_count)=j;
+        end
+      end
+    end
+  end 
+  
+  confusion_mat=zeros(n_classes,n_classes);
+  pred_class_vec=[];
+  output = zeros(n_classes,1);
+  all_tmp = [];
+  for i=1:n_classes
+    [test_set_start,test_set_end]=findTestSet(test_sizes,i);
+    test_set_size=test_set_end-test_set_start+1;
+    votes_i=votes(test_set_start:test_set_end,:);
+    for k=1:test_set_size
+      tmp=[];
+      for n=1:n_classes
+        tmp=[tmp sum(votes_i(k,:)==n)];
+      end
+      all_tmp=[all_tmp;tmp];
+    end
+  end
+  
+  accuracy=zeros(n_classes,1);
+  sum_all = [];
+  
+  for i=1:n_classes
+    sum_vec=[];
+    [test_set_start,test_set_end]=findTestSet(test_sizes,i);
+    test_set_size=test_set_end-test_set_start+1;
+    if test_set_size ~= 1
+      sum_vec  = sum(all_tmp(test_set_start:test_set_end,:));
+    else
+      sum_vec = all_tmp(i,:);
+    end
+    sum_all=[sum_all ; sum_vec];
+  end
+  
+  final_result = [];
+  for i=1:n_classes
+    [del,sortedIndices]=sort(sum_all(i,:),'descend');
+    top_sumK = sortedIndices(:,1:K);
+    final_result = [final_result ; top_sumK];
+    if sum(top_sumK==i) ~= 0
+      accuracy(i,1) = accuracy(i,1) + 1;
+    end
+  end
+  topKResult = final_result;
+  fprintf('Accuracy = %d/%d\n',sum(accuracy),n_classes);
+end 
