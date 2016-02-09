29099597da4cd832912b2740f3069247f6977787
Vaibhav Sharma <vaibhav@umn.edu>
2016-02-09 00:02:06 -0600

    Adding feature extraction scripts. These scripts extract features from printed on the Android logs and convert them into usable feature vectors

---
+++ feature_extraction/all_datacollect.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+./datacollect.sh --button;
+./datacollect.sh --checkbox;
+./datacollect.sh --radiobutton;
+./datacollect.sh --togglebutton;
+./datacollect.sh --switch;
+./datacollect.sh --spinner;
+./datacollect.sh --spinner-button;
+./datacollect.sh --edittext;
+./datacollect.sh --picker;
+
